a8010ced0ae98c6f7bb18664d1ebdcd55ab45a51
carraya <arrayacj@gmail.com>
2020-09-28 13:01:46 -0400

    updates lab2 and adds wordladder

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -9,7 +9,7 @@ def string_times(str, n):
 #Warmup-2 > front_times
 
 def front_times(str, n):
-  return str[:3]*n or str*n
+  return str[:3]*n
 
 #Warmup-2 > string_bits
 
@@ -19,12 +19,12 @@ def string_bits(str):
 #Warmup-2 > string_splosion
 
 def string_splosion(str):
-  return ''.join(str[:i] for i in range(1, len(str) + 1))
+  return ''.join(str[:i] for i in range(len(str)+1))
 
 #Warmup-2 > last2
 
 def last2(str):
-  return sum(1 for x in range(len(str)-1) if str[x:x+2] == str[-2:] and x != len(str)-2)
+  return sum(1 for i,e in enumerate(str) if str[i:i+2]==str[-2:] and i<len(str)-2)
 
 #Warmup-2 > array_count9
 
@@ -34,12 +34,12 @@ def array_count9(nums):
 #Warmup-2 > array_front9
 
 def array_front9(nums):
-  return  nums[:4].count(9)>=1 or len(nums)<4 and nums[:].count(9)>=1
+  return 9 in nums[:4]
 
 #Warmup-2 > array123
 
 def array123(nums):
-  return set([1,2,3]).issubset(nums)
+  return sum(1 for i in range(len(nums)-2) if nums[i:i+3]==[1,2,3])>0
 
 #Warmup-2 > string_match
 
@@ -69,17 +69,17 @@ def no_teen_sum(a, b, c):
 #Logic-2 > round_sum
 
 def round_sum(a, b, c):
-  return int(round(a,-1)+round(b,-1)+round(c,-1)) #second parameter is to what place you wanna round (-1 because not rounding would be ones place, -1 would be one place bigger which is tens place)
+  return (a+5)//10*10+(b+5)//10*10+(c+5)//10*10 
 
 #Logic-2 > close_far
 
 def close_far(a, b, c):
-  return (abs(abs(b)-abs(a))<=1 and abs(abs(c)-abs(a))>=2 or abs(abs(c)-abs(a))<=1 and abs(abs(b)-abs(a))>=2) and abs(abs(b)-abs(c))>=2 
+  return (abs(b-a)<=1 and abs(c-a)>=2 or abs(c-a)<=1 and abs(b-a)>=2) and abs(b-c)>=2
 
 #Logic-2 > make_chocolate
 
 def make_chocolate(small, big, goal):
-  return  goal - ((big if(5 * big <= goal) else goal / 5) * 5)  if small >= (goal - ((big if(5 * big <= goal) else goal / 5) * 5)) else -1
+  return [-1,max(goal-big*5,goal%5)][small>=max(goal-big*5,goal%5)]
 
 #String-2 > double_char
 
@@ -106,6 +106,11 @@ def count_code(str):
 def end_other(a, b):
   return a.lower().endswith(b.lower()) or b.lower().endswith(a.lower())
 
+#String-2 > xyz_there
+
+def xyz_there(str):
+  return str.count('xyz')!=str.count('.xyz')
+
 #List-2 > count_evens
 
 def count_evens(nums):
@@ -119,7 +124,7 @@ def big_diff(nums):
 #List-2 > centered_average
 
 def centered_average(nums):
-  return (sum(nums)-max(nums)-min(nums))/(len(nums)-2)
+  return (sum(nums)-max(nums)-min(nums))//(len(nums)-2)
 
 #List-2 > sum13
 
@@ -129,10 +134,9 @@ def sum13(nums):
 #List-2 > sum67
 
 def sum67(nums):
-  while 6 in nums: del nums[nums.index(6):nums[nums.index(6):].index(7)+nums.index(6)+1]
-  return sum(i for i in nums)
-
+  return sum([0, e][(nums[:i][::-1]+[6]).index(6)>=(nums[:i][::-1]+[7]).index(7) and e!=6] for i,e in enumerate(nums))
+  
 #List-2 > has22
 
 def has22(nums):
-  return sum(1 for x in range(len(nums)-1) if nums[x]==nums[x+1]==2)>=1 
+  return [i for i in range(len(nums)-1) if nums[i]==nums[i+1]==2]!=[]
---
+++ wordladder/wordladder.py
@@ -0,0 +1,148 @@
+import sys; args = sys.argv[1:]
+#Christopher Arraya Student, pd.6
+wl = set(open(args[0],'r').read().splitlines())
+import time;
+start = time.time()
+
+### FUNCTIONS ###
+
+def simpleGraphInfo(wl):
+    dic={}
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    for w in wl:
+        dic[w]=[]
+        for c in range(6):  
+            for l in range(26):
+                nw = w[:c] + alpha[l] + w[c+1:]
+                if nw in wl and nw!=w:  
+                    dic[w].append(nw)
+    count=0
+    for k in dic:
+        count+=len(dic[k])
+    ls = [0]
+    for k in dic:
+        degree = len(dic[k])
+        if len(ls)-1<degree:
+            ls+=[0]*(degree-(len(ls))+1)
+        ls[degree]+=1
+    print('Word count: ' + str(len(wl)))
+    print('Edge count: ' + str(count//2))    
+    print('Degree list: ' + ' '.join(map(str,ls)))
+    return dic
+
+graph = simpleGraphInfo(wl)
+
+def edgeCount(dic):
+    count = 0
+    for key in dic:
+        count+=len(dic[key])
+    return count//2
+
+def degreeList(dic):
+    ls = [0]
+    for k in dic:
+        degree = len(dic[k])
+        if len(ls)-1<degree:
+            ls+=[0]*(degree-(len(ls))+1)
+        ls[degree]+=1
+    return ls
+
+def secondDegree(dic):
+    count=0
+    dl=degreeList(graph)
+    for k in dic:
+        degree = len(dic[k])
+        if degree==len(dl)-2 and count==0:
+            return k
+    return None
+    
+def pathFromRootToGoal(dctSeen, start, end):
+    path=[] 
+    parseMe=[goal]
+    while parseMe:
+        item = parseMe.pop(0)
+        if item == '': return path[::-1]
+        parseMe.append(dctSeen[item])
+        path.append(item)
+    return []
+    
+def find_shortest_path(start, end):
+    dctSeen = {start: ''}
+    parseMe = [start]
+    while parseMe:
+        prnt = parseMe.pop(0)
+        if prnt==end: return pathFromRootToGoal(dctSeen, start, end)
+        for n in graph[prnt]:
+            if n not in dctSeen:   
+                parseMe.append(n) 
+                dctSeen[n] = prnt
+    return []
+
+def mark(recur, k, visited):
+    visited[k]=True
+    recur.append(k)
+    for i in graph[k]:
+        if visited[i]==False:
+            recur=mark(recur,i,visited)
+    return recur
+
+def ccList():
+    visited={}
+    sizes={}
+    kValues={2:0, 3:0, 4:0}
+    for k in graph:
+        visited[k]=False
+    for k in graph:
+        if visited[k]==False:
+            recur=[]
+            component = mark(recur, k, visited)
+            sizes[len(component)]='1'
+            if len(component)==2:
+                kValues[2]+=1
+            elif len(component)==3:
+                edges=0
+                for n in graph[k]:
+                    edges+=1
+                    for nn in graph[n]:
+                        edges+=1
+                if edges==6:
+                    kValues[3]+=1
+            elif len(component)==4:
+                edges=0
+                for n in graph[k]:
+                    edges+=1
+                    for nn in graph[n]:
+                        edges+=1
+                if edges==12:
+                    kValues[4]+=1
+    print('Connected component size count: ' + str(len(sizes)))
+    print('Largest component size: ' + str(max(sizes)))
+    print('K2 count: ' + str(kValues[2]))
+    print('K3 count: ' + str(kValues[3]))
+    print('K4 count: ' + str(kValues[4]))
+
+def farthest(root):
+    parseMe=[root]
+    path=[]
+    while parseMe:
+        prnt=parseMe.pop(0)
+        for n in graph[prnt]:
+            if n not in path:
+                parseMe.append(n)
+                path.append(n)
+    return path[-1]
+
+### CMD-LINE ARGS ###
+
+if len(args)==1: 
+    end = time.time()
+    print(f"Construction time: {round(end - start,3)}s")
+else:
+    end = time.time()
+    print(f"Construction time: {round(end - start,3)}s")
+    root = args[1]; goal = args[2] 
+    print('Second degree word: ' + secondDegree(graph))
+    ccList()
+    print('Neighbors: ' + str(graph[root]))
+    print('Farthest: ' + str(farthest(root)))
+    print('Path: ' + str(find_shortest_path(root,goal)))
